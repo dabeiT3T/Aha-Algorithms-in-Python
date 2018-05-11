63bf3e1213571e71db07fc486dced88f7018c188
dabeiT3T <dabei.status404@gmail.com>
2018-05-11 11:31:03 +0800

    C-1.py

---
+++ Sort/C-1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# read
+m = int(input('range (0~m): '))
+n = int(input('n: '))
+
+numbers = input('numbers: ')
+L = list(map(int, numbers.split(' ')))
+# bucket
+S = [0] * (m+1)
+# sort
+for num in L:
+    S[num] += 1
+# print
+# desc sort
+# for i in range(m, -1, -1):
+for i in range(m+1):
+    for j in range(S[i]):
+        print(i, end=' ')
+
+# new line
+print('')
+
+'''
+Sample:
+range (0~m): 100    <= sort num from 0~100
+n: 10               <= 5 nums to sort
+numbers: 8 100 50 22 15 6 1 1000 999 0
+                    <= nums splited with one space
+0 1 6 8 15 22 50 100 999 1000
+                    <= print answer
+'''
