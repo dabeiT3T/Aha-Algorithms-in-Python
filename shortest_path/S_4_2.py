8ea9b521843e6aaf83ca2a1015f02840c9a423e8
dabeiT3T <dabei.status404@gmail.com>
2018-11-20 16:11:51 +0800

    S_4

---
+++ shortest_path/S_4_2.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+'''
+5 7                 <= vertices, edges
+1 2 2               <= G
+1 5 10
+2 3 3
+2 5 7
+3 4 4
+4 5 5
+5 3 6
+0 2 5 9 9           <= print answer
+
+# Bellman-Ford optimized with queue.
+# here we use adjacency list with array
+# (which is list in python).
+'''
+
+# read
+_vertex, _edge = map(int, input().split())
+# G
+G = []
+for i in range(_edge):
+    u, v, w = map(int, input().split())
+    G.append((u-1, v-1, w))
+
+# suppose starting vertex
+startVertex = 0
+# starting shortest weight to other vertices
+dis = [float('inf') if i != startVertex else 0 for i in range(_vertex)]
+# stack
+L = []
+
+#init
+L.append(startVertex)
+
+while L:
+    vertex, *L = L
+    # or in [(u, v, w) for u, v, w if u == vertex]
+    for u, v, w in G:
+        if u == vertex:
+            if dis[v] > dis[u] + w:
+                dis[v] = dis[u] + w
+                if v not in L:
+                    L.append(v)
+
+# print
+print(*dis)
